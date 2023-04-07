5cfa6e34a8b7cf0cbaed1c22a5dbf4f2cf1855e9
David Han <shhan@sfu.ca>
2023-04-07 12:09:49 -0700

    Add listening comprehension mode

--- app/src/main/java/com/foxtrot/sudoku/controller/MainMenuActivity.java
+++ app/src/main/java/com/foxtrot/sudoku/controller/MainMenuActivity.java
@@ -6,10 +6,13 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.foxtrot.sudoku.R;
 import com.foxtrot.sudoku.model.BoardSize;
+import com.foxtrot.sudoku.model.GameMode;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     public static final String BOARD_SIZE_TAG = "board_size";
+
+    public static final String GAME_MODE = "game_mode";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,31 +22,43 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     private void displayMainMenuButtons() {
-        Button next4x4Button = (Button) findViewById(R.id.button_four);
+        Button next4x4Button = findViewById(R.id.button_four);
         next4x4Button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainMenuActivity.this, SudokuActivity.class);
+            Intent intent = new Intent(this, SudokuActivity.class);
             intent.putExtra(BOARD_SIZE_TAG, BoardSize._4X4.name());
+            intent.putExtra(GAME_MODE, GameMode.NORMAL.name());
             startActivity(intent);
         });
 
-        Button next6x6Button = (Button) findViewById(R.id.button_six);
+        Button next6x6Button = findViewById(R.id.button_six);
         next6x6Button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainMenuActivity.this, SudokuActivity.class);
+            Intent intent = new Intent(this, SudokuActivity.class);
             intent.putExtra(BOARD_SIZE_TAG, BoardSize._6X6.name());
+            intent.putExtra(GAME_MODE, GameMode.NORMAL.name());
             startActivity(intent);
         });
 
-        Button next9x9Button = (Button) findViewById(R.id.button_nine);
+        Button next9x9Button = findViewById(R.id.button_nine);
         next9x9Button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainMenuActivity.this, SudokuActivity.class);
+            Intent intent = new Intent(this, SudokuActivity.class);
             intent.putExtra(BOARD_SIZE_TAG, BoardSize._9X9.name());
+            intent.putExtra(GAME_MODE, GameMode.NORMAL.name());
             startActivity(intent);
         });
 
-        Button next12x12Button = (Button) findViewById(R.id.button_twelve);
+        Button next12x12Button = findViewById(R.id.button_twelve);
         next12x12Button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainMenuActivity.this, SudokuActivity.class);
+            Intent intent = new Intent(this, SudokuActivity.class);
             intent.putExtra(BOARD_SIZE_TAG, BoardSize._12X12.name());
+            intent.putExtra(GAME_MODE, GameMode.NORMAL.name());
+            startActivity(intent);
+        });
+
+        Button listeningComprehensionButton = findViewById(R.id.button_listening_comprehension);
+        listeningComprehensionButton.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SudokuActivity.class);
+            intent.putExtra(BOARD_SIZE_TAG, BoardSize._4X4.name());
+            intent.putExtra(GAME_MODE, GameMode.LISTENING_COMPREHENSION.name());
             startActivity(intent);
         });
     }
---
+++ app/src/main/java/com/foxtrot/sudoku/model/GameMode.java
@@ -0,0 +1,6 @@
+package com.foxtrot.sudoku.model;
+
+public enum GameMode {
+    NORMAL,
+    LISTENING_COMPREHENSION,
+}
